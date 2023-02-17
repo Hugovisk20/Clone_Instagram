cc5768a106d609b60a8146c44d54dbc7a401e9bc
Hugovisk20 <hugotavio2005@gmail.com>
2023-02-17 10:37:29 -0300

    Modify 'index.php' ;Add área notificações; Script
    Adicionado no index.php área do notificações do site; Add script retrair e retornar área de notificações.

--- ARQUIVOS/script_all.js
+++ ARQUIVOS/script_all.js
@@ -2,20 +2,36 @@
 let navPesquisa = document.getElementById('navPesquisa');
 function abrePesquisa(){
     if(navPesquisa.classList.contains('o')){
-        navPesquisa.style.width = '0px';
+        navPesquisa.style.transform = 'translateX(-300px)';
         navPesquisa.classList.remove('o');
         retornaNav();
     }else{
         navPesquisa.classList.add('o');
-        navPesquisa.style.width = '300px';
+        navPesquisa.style.transform = 'translateX(50px)';
         retraiNav();
     }
 }
 
 let navMenu = document.getElementById('navMenu');
 function retornaNav(){
+    navMenu.classList.remove('transformTeste');
     navMenu.style.width = '220px';
 }
 function retraiNav(){
+    navMenu.classList.add('transformTeste');
     navMenu.style.width = '50px';
+}
+
+//SCRIPT MOSTRA/ESCONDE NOTIFICAÇÕES
+let navNotificacao = document.getElementById('navNotificacao');
+function abreNotificacao(){
+    if(navNotificacao.classList.contains('o')){
+        navNotificacao.style.transform = 'translateX(-300px)';
+        navNotificacao.classList.remove('o');
+        retornaNav();
+    }else{
+        navNotificacao.classList.add('o');
+        navNotificacao.style.transform = 'translateX(50px)';
+        retraiNav();
+    }
 }
